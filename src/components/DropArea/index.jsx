257e33992386fbfb561b75c566dcbb883b266112
bojanabb <bojana.buljic@gmail.com>
2021-11-03 11:17:07 +0100

    Add drop functionality to DropArea component

--- src/components/DropArea/index.jsx
+++ src/components/DropArea/index.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { useDrop } from "react-dnd";
+import { lists } from "../../data";
 
-export default function DropArea({children}){
-
+export default function DropArea({children, onDrop, status}){
+    const [{isOver}, drop] = useDrop({
+        accept:"card",
+        canDrop: (card,monitor)=>{
+            const cardIndex = lists.findIndex(listItem => listItem.status === card.status);
+            const listIndex = lists.findIndex(listItem => listItem.status === status);
+            return [cardIndex+1, cardIndex-1, cardIndex].includes(listIndex)
+        },
+        drop:(card, monitor) => {
+            onDrop(card, monitor, status);
+        },
+        colect: monitor => ({
+            isOver:monitor.isOver()
+        })
+    });
     return(
-        <div className="list-wrapper">            
-            {React.cloneElement(children)}
+        <div ref={drop} className="list-wrapper">            
+            {React.cloneElement(children, {isOver})}
         </div>
     )
 }
