060c1b07e8b435ce35338038f708def2a99c5f77
bojanabb <bojana.buljic@gmail.com>
2021-11-03 10:15:28 +0100

    Update Title component

--- src/components/List/Title.jsx
+++ src/components/List/Title.jsx
@@ -1,9 +1,10 @@
-export default function Title(){
+
+export default function Title({list}){
 
     return(
         <div className="title-wrapper">
-            <h2 className="title">Backlog</h2>
-            <div className="title__color-bar" style={{background:"blue"}}/>
+            <h2 className="title">{list.status}</h2>
+            <div className="title__color-bar" style={{background: list.color}}/>
         </div>
     )
 }
